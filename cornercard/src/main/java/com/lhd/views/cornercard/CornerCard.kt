4b3efa1f513c34782243f6114f66228e3c57ff61
Nguyễn Văn Tiến <ngtien137@gmail.com>
2020-07-08 21:27:19 +0700

    add corner oval

--- cornercard/src/main/java/com/lhd/views/cornercard/CornerCard.kt
+++ cornercard/src/main/java/com/lhd/views/cornercard/CornerCard.kt
@@ -15,15 +15,6 @@ class CornerCard @JvmOverloads constructor(
     private val rectView = RectF()
     private val pathView = Path()
 
-    var cornerTopLeft = 0f
-        private set
-    var cornerTopRight = 0f
-        private set
-    var cornerBottomLeft = 0f
-        private set
-    var cornerBottomRight = 0f
-        private set
-
     private var shadowRadius = 0f
     private var shadowDx = 0f
     private var shadowDy = 0f
@@ -36,25 +27,19 @@ class CornerCard @JvmOverloads constructor(
 
     init {
         setLayerType(View.LAYER_TYPE_SOFTWARE, null)
+        if (parent is ViewGroup?) {
+            (parent as ViewGroup?)?.clipChildren = false
+        }
         setWillNotDraw(false)
         attrs?.let {
             val ta = context.obtainStyledAttributes(it, R.styleable.CornerCard)
-            cornerTopLeft =
-                ta.getDimensionPixelSize(R.styleable.CornerCard_cc_corner_top_left, 0).toFloat()
-            cornerTopRight =
-                ta.getDimensionPixelSize(R.styleable.CornerCard_cc_corner_top_right, 0).toFloat()
-            cornerBottomLeft =
-                ta.getDimensionPixelSize(R.styleable.CornerCard_cc_corner_bottom_left, 0)
-                    .toFloat()
-            cornerBottomRight =
-                ta.getDimensionPixelSize(R.styleable.CornerCard_cc_corner_bottom_right, 0)
-                    .toFloat()
 
             shadowRadius =
                 abs(ta.getDimensionPixelSize(R.styleable.CornerCard_cc_shadow_radius, 0).toFloat())
             shadowDx = ta.getDimensionPixelSize(R.styleable.CornerCard_cc_shadow_dx, 0).toFloat()
             shadowDy = ta.getDimensionPixelSize(R.styleable.CornerCard_cc_shadow_dy, 0).toFloat()
-            paintCard.color = ta.getColor(R.styleable.CornerCard_cc_background_color,Color.BLACK)
+            paintCard.color = ta.getColor(R.styleable.CornerCard_cc_background_color, Color.BLACK)
+
             shadowColor = ta.getColor(R.styleable.CornerCard_cc_shadow_color, Color.BLACK)
             if (shadowRadius > 0f) {
                 paintCard.setShadowLayer(
@@ -76,22 +61,18 @@ class CornerCard @JvmOverloads constructor(
         var leftOfView = rectView.left + shadowRadius + abs(shadowDx)
         var topOfView = rectView.top + shadowRadius + abs(shadowDy)
         val bottomOfView = rectView.bottom - shadowRadius - abs(shadowDy)
-        if (topOfView<rectView.top)
+        val viewHeight = bottomOfView - topOfView
+        val viewWidth = rightOfView - leftOfView
+        if (topOfView < rectView.top)
             topOfView = rectView.top
-        if (leftOfView<rectView.left)
+        if (leftOfView < rectView.left)
             leftOfView = rectView.left
-        pathView.moveTo(leftOfView, cornerTopLeft)
-        pathView.quadTo(leftOfView, topOfView, leftOfView + cornerTopLeft, topOfView)
-        pathView.lineTo(rightOfView - cornerTopRight, topOfView)
-        pathView.quadTo(rightOfView, topOfView, rightOfView, topOfView + cornerTopRight)
-        pathView.lineTo(rightOfView, bottomOfView - cornerBottomRight)
-        pathView.quadTo(
-            rightOfView, bottomOfView,
-            rightOfView - cornerBottomRight, bottomOfView
-        )
-        pathView.lineTo(leftOfView + cornerBottomLeft, bottomOfView)
-        pathView.quadTo(leftOfView, bottomOfView, leftOfView, bottomOfView - cornerBottomLeft)
-        pathView.lineTo(leftOfView, cornerTopLeft)
+        val leftOval = RectF(leftOfView, topOfView, leftOfView + viewHeight, topOfView + viewHeight)
+        val rightOval = RectF(rightOfView - viewHeight, leftOval.top, rightOfView, leftOval.bottom)
+        val centerRect = RectF(leftOval.centerX(), leftOval.top, rightOval.centerX(), leftOval.bottom)
+        pathView.addOval(leftOval, Path.Direction.CW)
+        pathView.addRect(centerRect, Path.Direction.CW)
+        pathView.addOval(rightOval, Path.Direction.CW)
     }
 
     override fun onDraw(canvas: Canvas?) {
